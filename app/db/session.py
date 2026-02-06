7f0cae9af7661fb96c4b454d5595e82865e93f54
SdebM <ser.v.mur@gmail.com>
2026-02-06 23:25:13 +0400

    Добавили управление сеансами базы данных с помощью SQLModel

---
+++ app/db/session.py
@@ -0,0 +1,18 @@
+from sqlmodel import SQLModel, Session, create_engine
+from typing import Generator
+
+from app.core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=settings.DEBUG, 
+    connect_args={"check_same_thread": False}
+)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
